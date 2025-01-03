ea9ab1de70eaf5f440a0a7d7e162e6a9aad0d48a
Jacob Felknor <jfelknor@firstrf.com>
2025-01-03 13:43:54 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+// This apparently is preferred by TypeScript because my typing is complete
+interface Button1Props {
+  startClick?: number;
+  startHover: number
+}
+const Button1: React.FC<Button1Props> = ({ startClick = 0, startHover }) => {
+  const [count, setCount] = useState({ click: startClick, hover: startHover })
+  if (count.click == 10) {
+    return <>You clicked too many times! <br></br> </>
+  } else {
 
-function Button() {
-  const [count, setCount] = useState({ click: 0, hover: 0 })
-  return (
-    <>
-      <button
-        onMouseEnter={() => { setCount({ click: count.click, hover: count.hover + 1 }) }}
-        onClick={() => { setCount({ click: count.click + 1, hover: count.hover }) }}
-      >
-        clicked {count.click} times
-        <br></br>
-        hovered {count.hover} times
-      </button>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-    </>
-  )
+    return (
+      <>
+        <button
+          onMouseEnter={() => { setCount({ click: count.click, hover: count.hover + 1 }) }}
+          onClick={() => { setCount({ click: count.click + 1, hover: count.hover }) }}
+        >
+          clicked {count.click} times
+          <br></br>
+          hovered {count.hover} times
+        </button>
+        <br />
+      </>
+    )
+  }
+}
+
+// This is a bit easier to read to my eye, and still Typed. 
+// I moved the default value from the function args to the variable assignment
+function Button2(props: Button1Props) {
+  const [count, setCount] = useState({ click: props.startClick ?? 0, hover: props.startHover })
+  if (count.click == 10) {
+    return <>You clicked too many times! <br></br> </>
+  } else {
+
+    return (
+      <>
+        <button
+          onMouseEnter={() => { setCount({ click: count.click, hover: count.hover + 1 }) }}
+          onClick={() => { setCount({ click: count.click + 1, hover: count.hover }) }}
+        >
+          clicked {count.click} times
+          <br></br>
+          hovered {count.hover} times
+        </button>
+        <br />
+      </>
+    )
+  }
+}
+
+// This is easiest to write (no interface), but I can't provide a type for `startHover` without giving it a default
+// value. If I want to require this prop, I have to leave it untyped which TypeScript isn't happy about (but works)
+function Button3({ startClick = 0, startHover }) {
+  const [count, setCount] = useState({ click: startClick, hover: startHover })
+  if (count.click == 10) {
+    return <>You clicked too many times! <br></br> </>
+  } else {
+
+    return (
+      <>
+        <button
+          onMouseEnter={() => { setCount({ click: count.click, hover: count.hover + 1 }) }}
+          onClick={() => { setCount({ click: count.click + 1, hover: count.hover }) }}
+        >
+          clicked {count.click} times
+          <br></br>
+          hovered {count.hover} times
+        </button>
+        <br />
+      </>
+    )
+  }
 }
 
 function App() {
@@ -36,7 +91,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button />
+        <Button1 startHover={1} />
+        <Button2 startHover={2} />
+        <Button3 startHover={3} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
